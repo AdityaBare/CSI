9e7795df05f30f8d03a904d79e9585d3b0c228c3
AdityaBare <bareaditya0@gmail.com>
2025-12-21 19:14:01 +0530

    server set

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,15 @@
 import express from "express";
 
+import cors from "cors";
 
 const app = express();
 const port = 8080;
+
+app.use(cors());
+app.use(express.urlencoded({extended:true,limit:"40kb"}));
+app.use(express.json({limit:"50kb"}));
+
+
+app.listen(port, ()=>{
+    console.log("Server is working on port :",port);
+}); 
