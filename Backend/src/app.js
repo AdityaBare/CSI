e07e49c690f4d134b24b5b37e229f820bfc2b258
AdityaBare <bareaditya0@gmail.com>
2025-12-21 19:28:55 +0530

    User Routes are set

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
-
 import cors from "cors";
+
+import userRoute from "./route/user.js";
 
 const app = express();
 const port = 8080;
@@ -9,7 +10,8 @@ app.use(cors());
 app.use(express.urlencoded({extended:true,limit:"40kb"}));
 app.use(express.json({limit:"50kb"}));
 
+app.use("/",userRoute);
 
-app.listen(port, ()=>{
+app.listen(port, async ()=>{
     console.log("Server is working on port :",port);
 }); 
