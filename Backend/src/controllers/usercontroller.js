a90c9e291cf683fcfc09173eb8860a6dbaf00260
AdityaBare <bareaditya0@gmail.com>
2025-12-23 09:40:20 +0530

    Cookies setup

--- Backend/src/controllers/usercontroller.js
+++ Backend/src/controllers/usercontroller.js
@@ -1,5 +1,5 @@
 import brycpt from "bcrypt";
-import crypto from  "crypto";
+import jwt from  "jsonwebtoken";
 import User from "../model/usermodel.js";
 import httpStatus from "http-status"
 
@@ -8,18 +8,28 @@ const login = async (req, res)=>{
     const {email , number , password} = req.body;
 
     try{
-        const user = await User.findOne({email,number});
+        const user = await User.findOne({email});
         if(!user){
             return res.status(httpStatus.NOT_FOUND).json({message:"User not found" , success:false});
         }
         const isMatch = await brycpt.compare(password,user.password);
         if(!isMatch){
-            return res.status(httpStatus.NOT_FOUND).json({message:"Password is incorrect",success:false});
+            return res.status(httpStatus.UNAUTHORIZED).json({message:"Invalid password",success:false});
         }
-        res.status(httpStatus.FOUND).json({message:"Login successfull", success:true});
+        const token = jwt.sign(
+        {userId:user._id},
+         process.env.JWT_SECRET,
+        { expiresIn: "5d" }
+      );
+            res.cookie("accessToken", token, {
+            httpOnly: true,
+            secure: false,        
+            sameSite: "lax",     
+            maxAge: 120*60 * 60 * 1000});
+        res.status(httpStatus.OK).json({message:"Login successfull", success:true});
 
     }catch(err){
-         res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message:`Internal server error : ${err}`, success:false});
+         res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message:`Internal server error : ${err.message}`, success:false});
 
     }
 }
@@ -30,8 +40,9 @@ const signup = async (req, res)=>{
 
     try{
         const existUser = await User.findOne({email});
+        console.log(existUser);
         if(existUser){
-            return res.status(httpStatus.FOUND).json({message:"User already exist.", success:false});
+            return res.status(httpStatus.CONFLICT).json({message:"User already exist.", success:false});
         }
 
         const hashPassword = await brycpt.hash(password,10);
@@ -45,12 +56,24 @@ const signup = async (req, res)=>{
         });
 
       await  user.save();
+      console.log(user);
+      const token = jwt.sign(
+        {userId:user._id},
+         process.env.JWT_SECRET,
+        { expiresIn: "5d" }
+      );
+      console.log(token);
+            res.cookie("accessToken", token, {
+            httpOnly: true,
+            secure: false,        
+            sameSite: "lax",     
+            maxAge: 120*60 * 60 * 1000});
 
       res.status(httpStatus.CREATED).json({message:"User is created", success:true});
 
 
     }catch(err){
-         res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message:`Internal server error : ${err}`, success:false});
+         res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message:` ${err.message}`, success:false});
 
 
     }
